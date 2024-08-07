a883225fafdca8c4ddece18225abf6f9aefb8d5b
federico <federicovongromann@gmail.com>
2024-08-07 14:17:49 -0300

    Improve components

--- src/view/quiz/Quiz.tsx
+++ src/view/quiz/Quiz.tsx
@@ -1,14 +1,10 @@
 import { ChangeEvent, useState, useEffect } from 'react';
 import type { Answer } from '../../data/questions';
 import AnswerOption from './AnswerOption';
-import { Button } from '../common/Button';
 import LazyImage from '../common/LazyImage';
-import MainTitle from '../common/MainTitle';
-import Title from '../common/Title';
+import Spinner from '../common/Spinner';
 import '../../styles/quiz.css';
 import '../../styles/button.css';
-import Spinner from '../common/Spinner';
-import FooterPanel from '../common/FooterPanel';
 
 interface PropsQuiz {
   genre: string;
@@ -27,28 +23,11 @@ interface PropsQuiz {
   handleInputField: (event: ChangeEvent<HTMLInputElement>) => void;
 }
 
-const Quiz = ({
-  genre,
-  title,
-  description,
-  result,
-  backgroundImageUrl,
-  checkedAnswers,
-  answerOptions,
-  errorAnswer,
-  questionId,
-  question,
-  setNextSlide,
-  setNewQuiz,
-  handleAnswerSelected,
-  handleInputField,
-}: PropsQuiz) => {
+const Quiz = ({ genre, description, result, backgroundImageUrl, checkedAnswers, answerOptions, questionId, handleAnswerSelected, handleInputField }: PropsQuiz) => {
   const [imageLoaded, setImageLoaded] = useState(false);
   const [loading, setLoading] = useState(true);
 
   const typeBackgroundImageUrl = !result ? backgroundImageUrl : `/${result}.jpg`;
-  const typeContent = !result ? question : `You Prefer ${result}!`;
-  const typeTitle = !result ? title : ``;
 
   const panelContainerStyle = {
     backgroundImage: imageLoaded ? `url(${typeBackgroundImageUrl})` : 'none',
@@ -66,12 +45,6 @@ const Quiz = ({
 
   return (
     <div key={questionId} className='quiz-container'>
-      <MainTitle className={'question-panel'}>
-        <Title className={'question'} content={typeContent} />
-      </MainTitle>
-      <MainTitle className={'title-panel'}>
-        <h3>{typeTitle}</h3>
-      </MainTitle>
       <div className={'panel-container'} style={panelContainerStyle}>
         {loading && <Spinner className={'loading-spinner'} />}
         <LazyImage
@@ -103,10 +76,6 @@ const Quiz = ({
             </ul>
           </>
         )}
-      </div>
-      <FooterPanel className={'footer-panel'} content={errorAnswer} />
-      <div className={'button-panel'}>
-        {!result ? <Button className={'button-next'} onClick={setNextSlide} title={'Next Slide'}></Button> : <Button className={'button-next'} onClick={setNewQuiz} title={'New Quiz!'}></Button>}
       </div>
     </div>
   );
--- src/view/quiz/QuizContainer.tsx
+++ src/view/quiz/QuizContainer.tsx
@@ -8,6 +8,10 @@ import { useAnswerHandlers } from '../../hooks/useAnswerHandlers';
 import { defaultGenre } from '../../data/questions';
 import QuestionCount from './QuestionCount';
 import '../../styles/quizContainer.css';
+import MainTitle from '../common/MainTitle';
+import Title from '../common/Title';
+import { Button } from '../common/Button';
+import FooterPanel from '../common/FooterPanel';
 
 const QuizContainer = () => {
   const { result, getResults, setResults } = useResult();
@@ -22,8 +26,10 @@ const QuizContainer = () => {
     setAnswerConfig,
   });
 
-  const { filterAnwserOpt, genre } = answerConfig;
-  const { counter, questionId } = questionConfig;
+  const { filterAnwserOpt, genre, errorAnswer } = answerConfig;
+  const { counter, questionId, question, title } = questionConfig;
+  const typeContent = !result ? question : `You Prefer ${result}!`;
+  const typeTitle = !result ? title : ``;
   const questionTotal = filterAnwserOpt.length;
   return (
     <div>
@@ -39,7 +45,19 @@ const QuizContainer = () => {
           </>
         )}
       </div>
-      <Quiz {...{ ...answerConfig, ...questionConfig, result, counter, questionTotal, setNextSlide, setNewQuiz, handleInputField, handleAnswerSelected }} />
+      <div key={questionId} className='quiz-container'>
+        <MainTitle className={'question-panel'}>
+          <Title className={'question'} content={typeContent} />
+        </MainTitle>
+        <MainTitle className={'title-panel'}>
+          <h3>{typeTitle}</h3>
+        </MainTitle>
+        <Quiz {...{ ...answerConfig, ...questionConfig, result, counter, questionTotal, setNextSlide, setNewQuiz, handleInputField, handleAnswerSelected }} />
+        <FooterPanel className={'footer-panel'} content={errorAnswer} />
+        <div className={'button-panel'}>
+          {!result ? <Button className={'button-next'} onClick={setNextSlide} title={'Next Slide'}></Button> : <Button className={'button-next'} onClick={setNewQuiz} title={'New Quiz!'}></Button>}
+        </div>
+      </div>
     </div>
   );
 };
